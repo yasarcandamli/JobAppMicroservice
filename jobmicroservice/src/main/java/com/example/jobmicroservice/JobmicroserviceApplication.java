b76daa44176127902a8a5d877a82660943094300
yasarcandamli <yasarcandamli@gmail.com>
2025-02-12 11:33:09 +0300

    Job Microservice Created

---
+++ jobmicroservice/src/main/java/com/example/jobmicroservice/JobmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmicroserviceApplication.class, args);
+	}
+
+}
