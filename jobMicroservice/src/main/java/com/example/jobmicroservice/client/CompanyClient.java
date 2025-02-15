5c46bbd9556ae3e1ed513e262000ca1117ddb5e7
yasarcandamli <yasarcandamli@gmail.com>
2025-02-15 15:36:57 +0300

    API Gateway added, Configured Spring Cloud Gateway for routing requests to COMPANY, JOB and REVIEW MICROSERVICES, Integrated Eureka for service discovery

--- jobMicroservice/src/main/java/com/example/jobmicroservice/client/CompanyClient.java
+++ jobMicroservice/src/main/java/com/example/jobmicroservice/client/CompanyClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "COMPANYMICROSERVICE")
+@FeignClient(name = "COMPANY-MICROSERVICE")
 public interface CompanyClient {
     @GetMapping("/companies/{id}")
     Company getCompany(@PathVariable("id") Long id);
--- jobMicroservice/src/main/java/com/example/jobmicroservice/client/ReviewClient.java
+++ jobMicroservice/src/main/java/com/example/jobmicroservice/client/ReviewClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "REVIEWMICROSERVICE")
+@FeignClient(name = "REVIEW-MICROSERVICE")
 public interface ReviewClient {
     @GetMapping("/reviews")
     List<Review> getReviews(@RequestParam("companyId") Long companyId);
