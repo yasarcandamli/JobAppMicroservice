b7bb525b110b531f5f625ff0cfc158aa4393fede
yasarcandamli <yasarcandamli@gmail.com>
2025-02-28 16:18:11 +0300

    Dockerized all microservices

--- jobMicroservice/src/main/java/com/example/jobmicroservice/client/CompanyClient.java
+++ jobMicroservice/src/main/java/com/example/jobmicroservice/client/CompanyClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "COMPANY-MICROSERVICE")
+@FeignClient(name = "COMPANY-MICROSERVICE", url = "${company-service.url}")
 public interface CompanyClient {
     @GetMapping("/companies/{id}")
     Company getCompany(@PathVariable("id") Long id);
--- jobMicroservice/src/main/java/com/example/jobmicroservice/client/ReviewClient.java
+++ jobMicroservice/src/main/java/com/example/jobmicroservice/client/ReviewClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "REVIEW-MICROSERVICE")
+@FeignClient(name = "REVIEW-MICROSERVICE", url = "${review-service.url}")
 public interface ReviewClient {
     @GetMapping("/reviews")
     List<Review> getReviews(@RequestParam("companyId") Long companyId);
